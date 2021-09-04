0f1ce45a102a080d74571c201eb760e6504ca0e5
Ardanrestun <ardanrn67@gmail.com>
2021-09-04 16:43:47 +0700

    Revert "Delete index.php"
    This reverts commit d5b1183726067c9cd3e6389e4c8ca3dd55909549.

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Nambah - Agen Beras</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/logonambah.png" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-expand-sm navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand" href="#page-top"><img src="assets/logonambah.png" alt="Nambah - Semua Kebagian"/></a>
+        </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead op">
+        <div class="container">
+            <div class="column">
+                <div class="row text-center mb-5">
+                    <div class="col-md-6">
+                        <div class="masthead-heading text-uppercase">Tertarik Menjadi Agen Beras?</div>
+                        <br>
+                        <a class="btn btn-primary btn-xl text-uppercase" href="#contact">Daftar Sekarang</a>
+
+                    </div>
+                    <div class="col-md-6">
+                        <img src="assets/nambah_packing_5.png" alt="Nambah - Semua Kebagian" width="300" height="400">
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <section class="page-section" id="contact">
+                        <div class="container">
+                            <div class="text-center">
+                                <h2 class="section-heading text-uppercase">Pendaftaran Agen Beras</h2>
+                                <h3 class="section-subheading text-muted">Banyak keuntungan yang didapat menjadi agen beras kami.</h3>
+                            </div>
+                            <!-- * * * * * * * * * * * * * * *-->
+                            <!-- * * SB Forms Contact Form * *-->
+                            <!-- * * * * * * * * * * * * * * *-->
+                            <!-- This form is pre-integrated with SB Forms.-->
+                            <!-- To make this form functional, sign up at-->
+                            <!-- https://startbootstrap.com/solution/contact-forms-->
+                            <!-- to get an API token!-->
+                            <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                                <div class="row align-items-stretch mb-5">
+                                    <div></div>
+                                    <div class="container text-center mb-3">
+                                        <div class="row align-items-center">
+                                            <div class="col-md-6 form-group text-center">
+                                                <!-- Name input-->
+                                                <h3>Nama</h3>
+                                                <p>*Sesuai KTP</p>
+                                            </div>
+                                            <div class="col-md-6 form-group align-items-center">
+                                                <!-- Name input-->
+                                                <div class="row">
+                                                    <input class="form-control" id="name" type="text" placeholder="Nama Anda" data-sb-validations="required"/>
+                                                    <div class="invalid-feedback" data-sb-feedback="name:required">Data Belum Terisi.</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="container text-center mb-3">
+                                        <div class="row align-items-center">
+                                            <div class="col-md-6 form-group text-center">
+                                                <!-- Name input-->
+                                                <h3>No KTP</h3>
+                                            </div>
+                                            <div class="col-md-6 form-group align-items-center">
+                                                <!-- Name input-->
+                                                <div class="row">
+                                                    <input class="form-control" id="noktp" type="text" placeholder="No KTP (16 Digit)" data-sb-validations="required" maxlength="16"/>
+                                                    <div class="invalid-feedback" data-sb-feedback="noktp:required">Data Belum Terisi.</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="container text-center mb-3">
+                                        <div class="row align-items-center">
+                                            <div class="col-md-6 form-group text-center">
+                                                <!-- Name input-->
+                                                <h3>Foto KTP</h3>
+                                            </div>
+                                            <div class="col-md-6 form-group align-items-center">
+                                                <!-- Name input-->
+                                                <div class="row">
+                                                    <input class="form-control-file" id="fktp" type="file" data-sb-validations="required"/>
+                                                    <div class="invalid-feedback" data-sb-feedback="fktp:required">Data Belum Terisi.</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="container text-center mb-3">
+                                        <div class="row align-items-center">
+                                            <div class="col-md-6 form-group text-center">
+                                                <!-- Name input-->
+                                                <h3>No NPWP</h3>
+                                            </div>
+                                            <div class="col-md-6 form-group align-items-center">
+                                                <!-- Name input-->
+                                                <div class="row">
+                                                    <input class="form-control" id="npwp" type="text" placeholder="No NPWP (15 Digit)" data-sb-validations="required" maxlength="15"/>
+                                                    <div class="invalid-feedback" data-sb-feedback="npwp:required">Data Belum Terisi.</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="container text-center mb-3">
+                                        <div class="row align-items-center">
+                                            <div class="col-md-6 form-group text-center">
+                                                <!-- Name input-->
+                                                <h3>Foto NPWP</h3>
+                                            </div>
+                                            <div class="col-md-6 form-group align-items-center">
+                                                <!-- Name input-->
+                                                <div class="row">
+                                                    <input class="form-control-file" id="fnpwp" type="file" data-sb-validations="required"/>
+                                                    <div class="invalid-feedback" data-sb-feedback="fnpwp:required">Data Belum Terisi.</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="container text-center mb-3">
+                                        <div class="row align-items-center">
+                                            <div class="col-md-6 form-group text-center">
+                                                <!-- Name input-->
+                                                <h3>No HP</h3>
+                                            </div>
+                                            <div class="col-md-6 form-group align-items-center">
+                                                <!-- Name input-->
+                                                <div class="row">
+                                                    <input class="form-control" id="nohp" type="text" placeholder="No Hp" data-sb-validations="required" maxlength="20"/>
+                                                    <div class="invalid-feedback" data-sb-feedback="nohp:required">Data Belum Terisi.</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="container text-center mb-3">
+                                        <div class="row align-items-center">
+                                            <div class="col-md-6 form-group text-center">
+                                                <!-- Name input-->
+                                                <h3>Email</h3>
+                                            </div>
+                                            <div class="col-md-6 form-group align-items-center">
+                                                <!-- Name input-->
+                                                <div class="row">
+                                                    <input class="form-control" id="email" type="email" placeholder="Email Anda " data-sb-validations="required" />
+                                                    <div class="invalid-feedback" data-sb-feedback="email:required">Data Belum Terisi.</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="container text-center mb-3">
+                                        <div class="row align-items-center">
+                                            <div class="col-md-6 form-group text-center">
+                                                <!-- Name input-->
+                                                <h3>Alamat</h3>
+                                                <p>*Sesuai KTP</p>
+                                            </div>
+                                            <div class="col-md-6 form-group align-items-center">
+                                                <!-- Name input-->
+                                                <div class="form-group form-group-textarea mb-md-0">
+                                           
+                                                <textarea class="form-control" id="alamat" placeholder="Alamat Anda" data-sb-validations="required"></textarea>
+                                                 <div class="invalid-feedback" data-sb-feedback="alamat:required">Data Belum Terisi.</div>
+                                            </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                
+                                    
+                                    <!-- <div class="col-md-6">
+                                        <div class="form-group form-group-textarea mb-md-0">
+                                           
+                                            <textarea class="form-control" id="message" placeholder="Your Message *" data-sb-validations="required"></textarea>
+                                            <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                                        </div>
+                                    </div> -->
+                                </div>
+                                <!-- Submit success message-->
+                                <!---->
+                                <!-- This is what your users will see when the form-->
+                                <!-- has successfully submitted-->
+                                <div class="d-none" id="submitSuccessMessage">
+                                    <div class="text-center text-white mb-3">
+                                        <div class="fw-bolder">Berhasil Mengirim Data</div>
+                                        Data anda akan kami proses segera.
+                                        <br />
+                                        care@nambah.id
+                                    </div>
+                                </div>
+                                <!-- Submit error message-->
+                                <!---->
+                                <!-- This is what your users will see when there is-->
+                                <!-- an error submitting the form-->
+                                <div class="d-none" id="submitErrorMessage">
+                                    <div class="text-center text-danger mb-3">Gagal Mengirim Data , Jika ada kendala hubungi WA 08xxxxxxxxx</div>
+                                </div>
+                                <!-- Submit Button-->
+                                <div class="text-center"><button class="btn btn-primary btn-xl text-uppercase disable" id="submitButton" type="submit">Send Message</button></div>
+                            </form>
+                        </div>
+                    </section>
+
+                </div>
+            </div>
+            <!-- <div class="masthead-subheading">Welcome To Our Studio!</div>
+                <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
+                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a> -->
+        </div>
+    </header>
+    <footer class="footer py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-4 text-lg-start">Copyright &copy; 2021</div>
+                <div class="col-lg-4 my-3 my-lg-0">
+                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-instagram"></i></a>
+                </div>
+                <div class="col-lg-4 text-lg-end">
+                    <a class="link-dark text-decoration-none me-3" href="#!">Kontak</a>
+                    <a class="link-dark text-decoration-none" href="#!">Tentang Kami</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+
+</html>
